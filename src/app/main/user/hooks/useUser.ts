6a420ffab25f89bea254021f9c64d0b592b2a5e5
Kasum <kasum2605@mail.ru>
2024-10-29 16:03:45 +0400

    asd

--- src/app/main/user/hooks/useUser.ts
+++ src/app/main/user/hooks/useUser.ts
@@ -5,7 +5,7 @@ import UserService from '@/services/user.service'
 export default function useUser(userId: number) {
 	const { data, isLoading, isFetching } = useQuery({
 		queryKey: ['user', { userId: userId }],
-		queryFn: () => UserService.getUser(6640542382),
+		queryFn: () => UserService.getUser(userId),
 		gcTime: 100000,
 	})
 
